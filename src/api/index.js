d49b491e5071980b954d9f6135eabf275ef8da01
Kenton <700brennan@gmail.com>
2023-02-12 13:36:37 -0600

    added body content with a scroll bar and updated config file

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,13 @@
 import config from '../config'
 
 export const GetAllPosts = () => {
-
+    fetch(`${config.apiLink}/${config.cohort}/posts`, {
+        method: 'Get',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ })
+    })
 }
 
 export const GetPost = (id) => {
---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+
+export const config = {
+    'apiLink' : 'https://strangers-things.herokuapp.com/api',
+    'cohort' : '2211-ftb-et-web-pt',   
+}
+
+// console.log(`${config.apiLink}/${config.cohort}/posts`)
